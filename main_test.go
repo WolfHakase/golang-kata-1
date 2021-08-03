0befbcb7b3123cb0de867d4b68681d1eeeede4ee
Nick Henneke <nickyhenneke+github@gmail.com>
2021-08-03 12:34:03 +0200

    Removed unneeded comments from test

--- main_test.go
+++ main_test.go
@@ -8,12 +8,8 @@ import (
 func TestWelcomeMessage(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	// given
 	expected := "Hello world!"
-
-	// than
 	actual := welcomeMessage()
 
-	// that
 	g.Expect(actual).To(Equal(expected))
 }
